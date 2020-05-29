1f07c7a66a3963fa2fad984b2dfd89c0b73b7399
Alex van Steenhoven <37883414+AlexVanSteenhoven@users.noreply.github.com>
2020-05-29 12:56:27 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Coptermail | daar blijf je voor thuis!</title>
+</head>
+<body>
+  <?php include_once 'includes/partials/navbar.inc.html';?>
+  <div class="content">
+    <!-- Hier alles Plaatsen -->
+  </div>
+  <?php include_once 'includes/partials/footer.inc.html';?>
+</body>
+</html>
